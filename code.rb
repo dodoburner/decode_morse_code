af83b509cac8e72813e7cd72f0a4f2d090a7b813
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-09-22 12:19:49 +0200

    Fix linters

--- code.rb
+++ code.rb
@@ -1,52 +1,52 @@
 @morse_alphabet = {
-  ".-" => "A",
-  "-..." => "B",
-  "-.-." => "C",
-  "-.." => "D",
-  "." => "E", 
-  "..-." => "F", 
-  "--." => "G", 
-  "...." => "H", 
-  ".." => "I",
-  ".---" => "J", 
-  "-.-" => "K", 
-  ".-.." => "L",
-  "--" => "M", 
-  "-." => "N", 
-  "---" => "O", 
-  ".--." => "P", 
-  "--.-" => "Q", 
-  ".-." => "R", 
-  "..." => "S", 
-  "-" => "T", 
-  "..-" => "U", 
-  "...-" => "V", 
-  ".--" => "W", 
-  "-..-" => "X", 
-  "-.--" => "Y", 
-  "--.." => "Z",
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z',
 }
 
 def decode_morse_char(char)
-  return @morse_alphabet[char]
+  @morse_alphabet[char]
 end
 
 def decode_morse_word(word)
-  decoded_word = ""
-  arr = word.split(" ")
-  arr.each{ |n| decoded_word += decode_morse_char(n) }
-  return decoded_word
+  decoded_word = ''
+  arr = word.split
+  arr.each { |n| decoded_word += decode_morse_char(n) }
+  decoded_word
 end
 
 def decode_morse_message(message)
-  decoded_message = ""
-  arr = message.split("   ")
+  decoded_message = ''
+  arr = message.split('   ')
   arr.each_with_index do |n, index|
-    if index == arr.length - 1
-      decoded_message += decode_morse_word(n)
+    decoded_message += if index == arr.length - 1
+      decode_morse_word(n)
     else
-      decoded_message += "#{decode_morse_word(n)} "
+      "#{decode_morse_word(n)} "
     end
   end
-  return decoded_message
+  decoded_message
 end
