865695a7f49662bd4fba64d211b22ac22bc61dfa
Bria222 <techconsultants.bree@gmail.com>
2022-09-22 11:21:28 +0300

    add morse method

--- code.rb
+++ code.rb
@@ -37,3 +37,16 @@ def decode_morse_word(word)
   arr.each{ |n| decoded_word += decode_morse_char(n) }
   return decoded_word
 end
+
+def decode_morse_message(message)
+  decoded_message = ""
+  arr = message.split("   ")
+  arr.each_with_index do |n, index|
+    if index == arr.length - 1
+      decoded_message += decode_morse_word(n)
+    else
+      decoded_message += decode_morse_word(n) + " "
+    end
+  end
+  return decoded_message
+end
